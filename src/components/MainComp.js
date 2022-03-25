572940c0470e697f36beff17997e919a5ceaf724
mtbishop <bishopmatthew76@gmail.com>
2022-03-25 14:28:16 -0600

    simple fade in and fade out animation for all 3 sections

--- src/components/MainComp.js
+++ src/components/MainComp.js
@@ -3,17 +3,34 @@ import HeaderNav from './HeaderNav';
 import AboutMe from './AboutMe';
 import Skills from './Skills';
 import Projects from './Projects';
-// import ParticleBackground from './ParticleBackground';
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 import '../styles/components/MainComp.scss';
 
 const Main = () => {
   return (
     <div className="main-container">
       <HeaderNav />
-      <AboutMe />
-      <Skills />
-      <Projects />
-      {/* <ParticleBackground /> */}
+      <AnimationOnScroll
+        duration={1}
+        animateIn="animate__fadeIn"
+        animateOut="animate__fadeOut"
+      >
+        <AboutMe />
+      </AnimationOnScroll>
+      <AnimationOnScroll
+        duration={1}
+        animateIn="animate__fadeIn"
+        animateOut="animate__fadeOut"
+      >
+        <Skills />
+      </AnimationOnScroll>
+      <AnimationOnScroll
+        duration={1}
+        animateIn="animate__fadeIn"
+        animateOut="animate__fadeOut"
+      >
+        <Projects />
+      </AnimationOnScroll>
     </div>
   );
 };
